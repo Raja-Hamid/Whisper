bff2dab041ea6afe4cad9a951a7cf30f2f3d8182
Raja-Hamid <rajahamidrazzaq24@gmail.com>
2024-06-03 20:11:05 +0500

    const

--- lib/screens/addUser_screen.dart
+++ lib/screens/addUser_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whisper/screens/chatPage_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:whisper/widgets/text_field.dart';
 
 class AddUserScreen extends StatefulWidget {
   final String currentUser;
@@ -13,8 +14,10 @@ class AddUserScreen extends StatefulWidget {
 }
 
 class _AddUserScreenState extends State<AddUserScreen> {
-  final textAddNameController = TextEditingController();
-  final textAddEmailController = TextEditingController();
+  String? email;
+  final emailController = TextEditingController();
+  final _formAddKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,109 +28,69 @@ class _AddUserScreenState extends State<AddUserScreen> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 20),
-              const CircleAvatar(
-                radius: 130,
-                backgroundImage: AssetImage('assets/images/icon.png'),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Whisper a friend',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: TextField(
-                  controller: textAddNameController,
-                  decoration: InputDecoration(
-                    labelText: 'User Name',
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.black12,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.black12,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+        child: Form(
+          key: _formAddKey,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 30),
+                const CircleAvatar(
+                  radius: 130,
+                  backgroundImage: AssetImage('assets/images/icon.png'),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: TextField(
-                  controller: textAddEmailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.black12,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.black12,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Whisper a friend',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  /* ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Upload Profile Picture',style: TextStyle(color: Colors.white),),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                    ),
-                  ),
-                  SizedBox(width: 20,),*/
-                  ElevatedButton(
-                    onPressed: () {
-                      Map<String, String> adduser = {
-                        'CurrentUser': widget.currentUser,
-                        'AddedUser': textAddNameController.text,
-                        'AddedEmail': widget.currentEmail
-                      };
-                      FirebaseFirestore.instance
-                          .collection('added_users')
-                          .add(adduser);
-
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ChatPage(
-                                  currentUser: widget.currentUser,
-                                  email: widget.currentEmail)));
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: CustomTextField(
+                    label: 'Email',
+                    hintText: 'Enter Email',
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Email';
+                      }
+                      return null;
                     },
-                    child: const Text(
-                      'Add',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 65, vertical: 10),
-                    ),
+                    controller: emailController,
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    Map<String, String> adduser = {
+                      'CurrentUser': widget.currentUser,
+                      'AddedEmail': widget.currentEmail
+                    };
+                    FirebaseFirestore.instance
+                        .collection('added_users')
+                        .add(adduser);
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ChatPage(
+                                currentUser: widget.currentUser,
+                                email: widget.currentEmail)));
+                  },
+                  child: Text(
+                    'Send Code',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 65, vertical: 10),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
